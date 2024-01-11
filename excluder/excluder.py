ba3a92479cf3c1cb2b087ebfbc13ea323607bf82
RedOctober117 <dbcahilly3@gmail.com>
2024-01-11 12:31:15 -0500

    generalized for non-unix and fixed redundant digging

--- excluder/excluder.py
+++ excluder/excluder.py
@@ -3,31 +3,36 @@ import sys, re
 
 def main():
     args = sys.argv[1:]
-    # inputs = tuple(input('Enter terms to search for, separated by spaces: ').split(' '))
-    pattern = '(?:' + '|'.join([ i for i in input('Enter terms to search for, separated by spaces: ').split(' ') ]) + ')'
-    print(f'Searching {args} for {pattern}')
+
+    pattern = re.compile('(?:' + '|'.join([ re.escape(i) for i in input('Enter terms to search for, separated by spaces: ').split(' ') ]) + ')')
+    print(f'Searching {args} for {pattern.pattern}')
     excludes = set()
+
     for path in args:
         cwd = Path(Path.home(), path)
         dig_path(cwd, excludes, pattern)
-    # print(excludes)
 
-    print('Writing to \'generated_excludes.txt\'')
-    with open('generated_excludes.txt', 'w') as exclude_txt:
+    write_excludes(Path.cwd(), 'generated_excludes.txt', excludes)
+
+
+def write_excludes(cwd, file, excludes):
+    print(f'Writing to \'{file}\'')
+    with open(file, 'w') as exclude_txt:
         for line in excludes:
-            # print(line)
             exclude_txt.write(line.as_posix() + '\n')
+    print(f'File writen at {Path(cwd, file)}')
 
 def dig_path(cwd, excludes, pattern):
-    # print(f'Scanning {cwd}')
     try:
-        to_be_excluded = [ Path(cwd.root, cwd) for i in cwd.iterdir() if re.match(pattern, i.name) != None ]
-        if len(to_be_excluded) > 0:
-            excludes.update(to_be_excluded)
-        for subdir in [ i for i in cwd.iterdir() if i.is_dir() and i not in [i.name for i in to_be_excluded ] ]:
+        anchored_path = Path(cwd.root, cwd)
+        for item in cwd.iterdir():
+            if pattern.match(item.name) != None:
+                excludes.add(anchored_path)
+                break
+        for subdir in [ i for i in cwd.iterdir() if i.is_dir() and anchored_path not in excludes ]:
             dig_path(subdir, excludes, pattern)
     except: 
-        print(f'ERROR: BROKEN AT {cwd.relative_to('/').as_posix()}')
+        print(f'ERROR: BROKEN AT {anchored_path.as_posix()}')
 
 
 if __name__ == '__main__':
