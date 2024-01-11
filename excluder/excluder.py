deae974091a408fd9f0cb30f43a70afb270e41c9
RedOctober117 <dbcahilly3@gmail.com>
2024-01-11 09:39:56 -0500

    updated path naming

--- excluder/excluder.py
+++ excluder/excluder.py
@@ -5,23 +5,23 @@ def main():
     args = sys.argv[1:]
     # inputs = tuple(input('Enter terms to search for, separated by spaces: ').split(' '))
     pattern = '(?:' + '|'.join([ i for i in input('Enter terms to search for, separated by spaces: ').split(' ') ]) + ')'
-    print(args)
-    print(pattern)
+    print(f'Searching {args} for {pattern}')
     excludes = set()
     for path in args:
         cwd = Path(Path.home(), path)
         dig_path(cwd, excludes, pattern)
-    print(excludes)
+    # print(excludes)
 
+    print('Writing to \'generated_excludes.txt\'')
     with open('generated_excludes.txt', 'w') as exclude_txt:
         for line in excludes:
-            print(line)
+            # print(line)
             exclude_txt.write(line.as_posix() + '\n')
 
 def dig_path(cwd, excludes, pattern):
-    print(f'Scanning {cwd}')
+    # print(f'Scanning {cwd}')
     try:
-        to_be_excluded = [ Path('/', cwd.relative_to('/')) for i in cwd.iterdir() if re.match(pattern, i.name) != None ]
+        to_be_excluded = [ Path(cwd.root, cwd) for i in cwd.iterdir() if re.match(pattern, i.name) != None ]
         if len(to_be_excluded) > 0:
             excludes.update(to_be_excluded)
         for subdir in [ i for i in cwd.iterdir() if i.is_dir() and i not in [i.name for i in to_be_excluded ] ]:
